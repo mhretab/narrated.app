21b2f477fa030e98a6e1066eb117b95dbee05d31
Mhretab Kidane <mhretab@users.noreply.github.com>
2025-03-03 07:58:45 +0100

    feat: email verfication

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,6 +1,9 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '@/server/db';
+import { sendEmail } from '@/actions/email';
+import clientEnv from '@/lib/client-env';
+import env from '@/lib/env';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -8,6 +11,18 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    autoSignInAfterVerification: true,
+    sendVerificationEmail: async ({ user, token }) => {
+      const verificationUrl = `${clientEnv.NEXT_PUBLIC_BETTER_AUTH_URL}/api/auth/verify-email?token=${token}&callbackURL=${env.EMAIL_VERIFICATION_CALLBACK_URL}`;
+      await sendEmail({
+        to: user.email,
+        subject: 'Verify your email address',
+        text: `Click the link to verify your email: ${verificationUrl}`,
+      });
+    },
   },
 });
 
